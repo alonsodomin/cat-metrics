91496bdc6ef881c232f65afb5e31691402ff76d2
Antonio Dominguez <antonio.dominguez@wejo.com>
2019-09-26 11:29:52 +0100

    Provide a stream of durations from the other instruments

--- src/main/scala/cats/metrics/Counter.scala
+++ src/main/scala/cats/metrics/Counter.scala
@@ -1,29 +1,39 @@
 package cats
 package metrics
 
-import cats.effect.Sync
+import cats.effect._
 import cats.effect.concurrent._
 import cats.syntax.functor._
+
+import fs2.Stream
+
+import scala.concurrent.duration._
 
 trait Counter[F[_]] extends Instrument[F, Long] {
   def increment(): F[Unit]
   def incrementBy(amount: Long): F[Unit]
+
+  def data: Stream[F, Long]
 }
 
 object Counter {
 
-  def apply[F[_]](implicit F: Sync[F]): F[Counter[F]] = startAt[F](0)
+  def apply[F[_]: Sync : Timer]: F[Counter[F]] = startAt[F](0)
 
-  def startAt[F[_]](initial: Long)(implicit F: Sync[F]): F[Counter[F]] =
+  def startAt[F[_]: Sync : Timer](initial: Long): F[Counter[F]] =
     Ref[F].of(initial).map(new Impl[F](initial, _))
 
-  private class Impl[F[_]](initial: Long, value: Ref[F, Long]) extends Counter[F] {
+  private class Impl[F[_]: Sync: Timer](initial: Long, value: Ref[F, Long]) extends Counter[F] {
     def increment(): F[Unit] = incrementBy(1)
     def incrementBy(amount: Long): F[Unit] =
       value.modify(curr => (curr + amount, ()))
 
     def peek(): F[Long] = value.get
     def reset(): F[Long] = value.getAndSet(initial)
+
+    def data: Stream[F, Long] = {
+      Stream.awakeDelay[F](5.seconds).evalMap(_ => value.get)
+    }
   }
 
 }
--- src/main/scala/cats/metrics/Gauge.scala
+++ src/main/scala/cats/metrics/Gauge.scala
@@ -1,23 +1,33 @@
 package cats
 package metrics
 
-import cats.effect.Sync
+import cats.effect.{Sync, Timer}
 import cats.effect.concurrent.Ref
 import cats.syntax.functor._
 
+import fs2.Stream
+
+import scala.concurrent.duration._
+
 trait Gauge[F[_], A] extends Instrument[F, A] {
   def set(value: A): F[Unit]
+
+  def data: Stream[F, A]
 }
 
 object Gauge {
-  def of[F[_]: Sync, A: Numeric](initial: A): F[Gauge[F, A]] =
+  def of[F[_]: Sync: Timer, A: Numeric](initial: A): F[Gauge[F, A]] =
     Ref[F].of(initial).map(new Impl[F, A](initial, _))
 
-  private class Impl[F[_], A: Numeric](initial: A, value: Ref[F, A]) extends Gauge[F, A] {
+  private class Impl[F[_]: Sync: Timer, A: Numeric](initial: A, value: Ref[F, A]) extends Gauge[F, A] {
     def set(v: A): F[Unit] = value.set(v)
 
     def peek(): F[A] = value.get
     def reset(): F[A] = value.getAndSet(initial)
+
+    def data: Stream[F, A] = {
+      Stream.awakeDelay[F](5.seconds).evalMap(_ => value.get)
+    }
   }
 
 }
